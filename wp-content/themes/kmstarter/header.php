396bb5a252c8264d422cc96a772554525b908e6c
karrine <karrinem@outlook.com>
2017-12-10 14:24:51 -0800

    display header

--- wp-content/themes/kmstarter/header.php
+++ wp-content/themes/kmstarter/header.php
@@ -24,6 +24,11 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'km_starter' ); ?></a>
 <!-- HEADER -->
+
+<figure class="header_image">
+<?php the_header_image_tag(); ?>
+</figure>
+
 	<header id="masthead" class="site-header">
 	<!-- .site-branding -->
 		<div class="site-branding">
--- wp-content/themes/kmstarter/inc/custom-header.php
+++ wp-content/themes/kmstarter/inc/custom-header.php
@@ -19,9 +19,9 @@
 function km_starter_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'km_starter_custom_header_args', array(
 		'default-image'          => '',
-		'default-text-color'     => '000000',
-		'width'                  => 1000,
-		'height'                 => 250,
+		'default-text-color'     => 'ffffff',
+		'width'                  => 2000,
+		'height'                 => 850,
 		'flex-height'            => true,
 		'wp-head-callback'       => 'km_starter_header_style',
 	) ) );
