1f00dd08aeccfbc3dd1c564fd0b8de613e777fba
Student <student@CVS-R107D015.nic.bc.ca>
2017-12-12 20:56:37 -0800

    move sidebar from single.php to content-single.php

---
+++ wp-content/themes/kmstarter/template-parts/content-single.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package km_starter
+ */
+
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<!-- Categorey List -->
+		<?php kmstarter_the_categorey_list(); ?>
+
+		<?php
+		if ( is_singular() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		endif;
+
+		if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php km_starter_posted_on(); ?>
+
+		</div><!-- .entry-meta -->
+		<?php
+		endif; ?>
+	</header><!-- .entry-header -->
+
+	<section class="post-content">
+	
+
+	<div class="entry-content">
+		<?php
+			the_content( sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'km_starter' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			) );
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'km_starter' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php km_starter_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+<?php 
+//Post Navigation 
+			km_starter_post_navigation();
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		?>
+		</section>
+	<?php get_sidebar(); ?>
+</article><!-- #post-<?php the_ID(); ?> -->
